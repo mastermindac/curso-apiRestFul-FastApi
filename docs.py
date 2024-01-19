204807d1f4cf1d118f2612c7accc6a2554b4cfed
Paco <paco@mastermind.ac>
2024-01-19 09:46:17 +0100

    Documentacion API

---
+++ docs.py
@@ -0,0 +1,7 @@
+#Tags necesarios para documentacion con OpenAPI
+tags_metadata = [
+    {
+        "name": "ingredientes",
+        "description": "Operaciones relacionadas con el CRUD de ingredientes",
+    },
+]
--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from docs import tags_metadata
 
 # Objeto app de tipo FastApi
-app = FastAPI()
+app = FastAPI(
+    title="FoodAPI",
+    description="ApiRestFul para la gestión de alimentos y planes nutricionales",
+    version="0.0.2",
+    contact={
+        "name":"Paco Gómez",
+        "url":"http://www.mastermind.ac"
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+    openapi_tags=tags_metadata
+)
 
 #Configuracion del ApiRestFul
 
@@ -11,8 +25,6 @@ def read_root():
     return {"Hola": "Pakito"}
 
 #Endpoint GET /ingredientes
-@app.get("/ingredientes")
+@app.get("/ingredientes",tags=["ingredientes"])
 def read_ingredients():
     return {"Objeto": "Ingredientes"}
-
-
